3bc64bd63889263bcad6b8d11f2526fd63f50a82
Martin Hellstrom <martin.hellstrom@dgc.se>
2018-11-13 14:42:37 +0100

    simplifies the script

--- run.sh
+++ run.sh
@@ -8,23 +8,7 @@ IFS=$'\n'
 
 sed -i '/date;description;url/d' out.txt
 
-for i in $(cat out.txt); do 
-	DATE=$(echo $i | cut -d ';' -f1)
-	DATE_CONVERTED=$(date -d "${DATE}" +%s)
-	echo $i | sed "s/^${DATE};/${DATE_CONVERTED};/" >> converted.txt
-done
+END=${END_DATE:-$(date +%Y-%m-%d)}
+START=${BEGIN_DATE:-$(date +%Y-%m-%d -d 'yesterday')}
 
-if [ -z ${BEGIN_DATE} ]; then
-	BEGIN_DATE=$(date -d "$(date +%Y-%m-%d -d 'yesterday')" +%s)
-else
-	BEGIN_DATE=$(date -d "${BEGIN_DATE}" +%s)
-fi
-
-if [ -z ${END_DATE} ]; then
-	END_DATE=$(date +%s)
-else
-	END_DATE=$(date -d "${END_DATE}" +%s)
-fi
-
-awk -v B_DATE=$BEGIN_DATE -v E_DATE=$END_DATE '$1 >=B_DATE && $1 <=E_DATE {print $0"\n"}' converted.txt
-
+awk -v B_DATE=${START} -v E_DATE=${END} '$1 >=B_DATE && $1 <=E_DATE {print $0"\n"}' out.txt
